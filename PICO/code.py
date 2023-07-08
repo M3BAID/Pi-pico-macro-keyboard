1fe1f5705c31dd43320500b0c322d0cf5ae043e2
Mohammed <51389957+M3BAID@users.noreply.github.com>
2023-07-09 02:54:58 +0300

    Add files via upload

---
+++ PICO/code.py
@@ -0,0 +1,123 @@
+import time
+import board
+import digitalio
+import usb_hid
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+keyboard = Keyboard(usb_hid.devices)
+
+btn1_pin = board.GP0
+btn1 = digitalio.DigitalInOut(btn1_pin)
+btn1.direction = digitalio.Direction.INPUT
+btn1.pull = digitalio.Pull.DOWN
+
+btn2_pin = board.GP1
+btn2 = digitalio.DigitalInOut(btn2_pin)
+btn2.direction = digitalio.Direction.INPUT
+btn2.pull = digitalio.Pull.DOWN
+
+btn3_pin = board.GP2
+btn3 = digitalio.DigitalInOut(btn3_pin)
+btn3.direction = digitalio.Direction.INPUT
+btn3.pull = digitalio.Pull.DOWN
+
+btn4_pin = board.GP3
+btn4 = digitalio.DigitalInOut(btn4_pin)
+btn4.direction = digitalio.Direction.INPUT
+btn4.pull = digitalio.Pull.DOWN
+
+btn5_pin = board.GP4
+btn5 = digitalio.DigitalInOut(btn5_pin)
+btn5.direction = digitalio.Direction.INPUT
+btn5.pull = digitalio.Pull.DOWN
+
+btn6_pin = board.GP5
+btn6 = digitalio.DigitalInOut(btn6_pin)
+btn6.direction = digitalio.Direction.INPUT
+btn6.pull = digitalio.Pull.DOWN
+
+btn7_pin = board.GP7
+btn7 = digitalio.DigitalInOut(btn7_pin)
+btn7.direction = digitalio.Direction.INPUT
+btn7.pull = digitalio.Pull.DOWN
+
+btn8_pin = board.GP8
+btn8 = digitalio.DigitalInOut(btn8_pin)
+btn8.direction = digitalio.Direction.INPUT
+btn8.pull = digitalio.Pull.DOWN
+
+btn9_pin = board.GP9
+btn9 = digitalio.DigitalInOut(btn9_pin)
+btn9.direction = digitalio.Direction.INPUT
+btn9.pull = digitalio.Pull.DOWN
+
+btn10_pin = board.GP10
+btn10 = digitalio.DigitalInOut(btn10_pin)
+btn10.direction = digitalio.Direction.INPUT
+btn10.pull = digitalio.Pull.DOWN
+
+btn11_pin = board.GP11
+btn11 = digitalio.DigitalInOut(btn11_pin)
+btn11.direction = digitalio.Direction.INPUT
+btn11.pull = digitalio.Pull.DOWN
+
+btn12_pin = board.GP12
+btn12 = digitalio.DigitalInOut(btn12_pin)
+btn12.direction = digitalio.Direction.INPUT
+btn12.pull = digitalio.Pull.DOWN
+while True:
+
+    
+    
+    
+    if btn1.value:
+        keyboard.send(Keycode.F13)
+        time.sleep(0.1)
+
+    if btn2.value:
+        keyboard.send(Keycode.F14)
+        time.sleep(0.1)
+
+    if btn3.value:
+        keyboard.send(Keycode.F15)
+        time.sleep(0.1)
+
+    if btn4.value:
+        keyboard.send(Keycode.F16)
+        time.sleep(0.1)
+
+    if btn5.value:
+        keyboard.send(Keycode.F17)
+        time.sleep(0.1)
+
+    if btn6.value:
+        keyboard.send(Keycode.F18)
+        time.sleep(0.1)
+
+    if btn7.value:
+        keyboard.send(Keycode.F19)
+        time.sleep(0.1)
+
+    if btn8.value:
+        keyboard.send(Keycode.F20)
+        time.sleep(0.1)
+
+    if btn9.value:
+        keyboard.send(Keycode.F21)
+        time.sleep(0.1)
+
+    if btn10.value:
+        keyboard.send(Keycode.F22)
+        time.sleep(0.1)
+
+    if btn11.value:
+        keyboard.send(Keycode.F23)
+        time.sleep(0.1)
+
+    if btn12.value:
+        keyboard.send(Keycode.F24)
+        time.sleep(0.1)
+     
+    time.sleep(0.1)
